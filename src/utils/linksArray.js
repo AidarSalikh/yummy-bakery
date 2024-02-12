4153ae7b1ea36aeda241b331df4f2ae06026f3d2
Aidar Salikh <aidarrrr95@gmail.com>
2024-02-12 18:06:16 +0300

    fixed problem with chrome

--- src/utils/linksArray.js
+++ src/utils/linksArray.js
@@ -1,8 +1,8 @@
 export const linksArray = [
+  { link: "/shop", name: "Все" },
   { link: "cakes", name: "Торты" },
   { link: "trifles", name: "Трайфалы" },
   { link: "merengue", name: "Меренговый" },
-  { link: "/shop", name: "Все" },
   { link: "trifles", name: "Трайфалы" },
   { link: "trifles", name: "Трайфалы" },
 ];
